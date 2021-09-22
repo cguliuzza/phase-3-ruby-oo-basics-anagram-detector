756b53807ebcc1762bde54cde51c9a24368a2379
christinaguliuzza <christina.guliuzza@gmail.com>
2021-09-21 20:20:11 -0700

    commit tree 551d417d8356c795684fa9e78fbe7a351ce7a73e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word, :match
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match
+        @match = self.match(%w[])
+    end
+end
+
+# listen = Anagram.new("listen")
+# listen.match(%w[enlists google inlets banana])
+
+#instances should respond to a match instance method that takes an array of possible anagrams. It should return all matches in an array. If no matches exist, it should return an empty array.
